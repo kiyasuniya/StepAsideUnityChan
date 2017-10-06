6bbd49d0b43707217777c022d85a0113e5f1413e
道厘花清 <taketori01plus64@ezweb.ne.jp>
2017-10-06 22:23:47 +0900

    StepAsideUnityChan　提出課題
    Lesson6の提出課題です

---
+++ Assets/DestroyController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyController : MonoBehaviour {
+
+	//Unityちゃんのオブジェクト
+	private GameObject unitychan;
+
+
+	// Use this for initialization
+	void Start () {
+		//Unityちゃんのオブジェクトを取得
+		this.unitychan = GameObject.Find ("unitychan");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Unityちゃんが通り過ぎたオブジェクトを１秒後に削除
+		if (this.unitychan.transform.position.z > this.transform.position.z) {
+			Destroy (gameObject, 1);
+			Debug.Log ("destroy");
+		}
+
+	}
+}
